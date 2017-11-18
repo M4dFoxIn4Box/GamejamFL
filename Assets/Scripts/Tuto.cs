c815e11f35cbf8ffa89db8ba0920ed2d821f8c7d
OTAKU <otaku.suprem@gmail.com>
2017-11-18 17:12:17 +0100

    input modif

---
+++ Assets/Scripts/Tuto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuto : MonoBehaviour
+{
+    public GameObject tuto;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Time.timeScale = 0;
+        tuto.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+        {
+            if(Input.GetButtonDown("grabButton"))
+            {
+            Time.timeScale = 1;
+            tuto.SetActive(false);
+            }
+        }
+		
+}
+
+
