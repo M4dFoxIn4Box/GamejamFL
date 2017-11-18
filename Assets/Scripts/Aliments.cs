908de5a359cfb65a3084c056fbb8055b6f1686c0
OTAKU <otaku.suprem@gmail.com>
2017-11-18 02:57:03 +0100

    Scripts des aliment + debut de score system

---
+++ Assets/Scripts/Aliments.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aliments : MonoBehaviour {
+
+    public enum AlimType
+    {
+        Fruit,
+        Legumes
+    }
+
+    public AlimType type;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour {
+
+    public Aliments.AlimType type;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Aliments aliment = other.gameObject.GetComponentInParent<Aliments>();
+        if(aliment.type == type)
+        {
+            if (other.gameObject.CompareTag("Blueteam"))
+            {
+
+            }
+        }
+    }
+}
