b568e969e3e2f0e28b54e820d5a63a026d3eb914
OTAKU <otaku.suprem@gmail.com>
2017-11-18 17:31:35 +0100

    Tuto settings

--- Assets/Scripts/TutoManager.cs
+++ Assets/Scripts/TutoManager.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tuto : MonoBehaviour
+public class TutoManager : MonoBehaviour
 {
     public GameObject tuto;
 
 	// Use this for initialization
 	void Start ()
     {
-        Time.timeScale = 0;
-        tuto.SetActive(true);
+       
 	}
 	
 	// Update is called once per frame
 	void Update () 
         {
-            if(Input.GetButtonDown("grabButton"))
+          if(Input.GetButtonDown("graButton"))
             {
-            Time.timeScale = 1;
-            tuto.SetActive(false);
+                Time.timeScale = 1;
             }
         }
-		
+	public void TutoLaunch ()
+    {
+       Time.timeScale = 0;
+       tuto.SetActive(true);
+    }
 }
 
 
